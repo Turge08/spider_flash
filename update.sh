a5f9578b4e784905194536d3c07ab83f5e5141a3
Turge <turge08@gmail.com>
2021-11-13 10:36:16 -0500

    Fixed update.sh

--- update.sh
+++ update.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+dir=$(pwd)
 cd ~/klipper
-cp ~/klipper_config/firmware/linux.config .config
+cp $dir/linux.config .config
+
+# raspberry pi
 make clean
 make
 sudo systemctl stop klipper
 make flash
-sudo systemctl start klipper
 
-cp ~/klipper_config/firmware/spider_uart.config .config
+# spider
+cp $dir/spider_uart.config .config
 make clean
 make
-
-sudo systemctl stop klipper
 /home/pi/klipper/scripts/flash-sdcard.sh -b 250000 /dev/ttyAMA0 fysetc-spider-v1
+
 sudo systemctl start klipper
